db5128775b2e19e5cc813824f2dd3c16191523bb
Purav Shah <purav.shah@resilinc.com>
2024-08-13 18:08:12 +0530

    Adding logging improvements in Index.js

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ try {
 
   const bomFile = core.getInput('bomfile');
   core.info('Security Analysis will be started with SBOM: ' + bomFile);
-  
+
   const bomContents = fs.readFileSync(bomFile, 'utf8').toString().substring(0, 200);
-  core.info('SBOM is available to be uploaded with Contents: ' + bomContents);
+  core.info('SBOM is available to be uploaded with Snippet: ' + bomContents + '...');
 
   core.info('Starting SBOM upload to Dependency-Track API Backend.');
   const multipartForm = new FormData();
@@ -23,23 +23,26 @@ try {
   multipartForm.append('bom', fs.createReadStream(bomFile));
 
   axios.post(core.getInput('apidomain') + '/api/v1/bom', multipartForm, {
-  headers: {
-    ...multipartForm.getHeaders(),
-    'X-API-Key': core.getInput('apikey')
-  }
-  })
-  .then(response => {
-  
-    core.info('SBOM upload has been successful: ' + response.data);
-  })
-  .catch(error => {
-  
-    core.error('Error while uploading SBOM: ' + error);
-    core.setFailed('Error in File or API request while uploading SBOM.');
+    headers: {
+      ...multipartForm.getHeaders(),
+      'X-API-Key': core.getInput('apikey')
+    }
+  }).then(response => {
+
+    const status = response.status;
+    const data = JSON.stringify(response.data);
+    if (status >= 200 && status < 400) {
+
+      core.info('SBOM upload has been successful: ' + data);
+    } else {
+
+      core.setFailed('Error while uploading SBOM with File or API request: ' + error.message);
+    }
+  }).catch(error => {
+
+    core.setFailed('Error while uploading SBOM with File or API request: ' + error.message);
   });
 } catch (error) {
 
-  const reason = error.message;
-  core.error('SBOM Processing Error: ' + reason);
-  core.setFailed(reason);
+  core.setFailed('Error while processing SBOM File: ' + error.message);
 }
